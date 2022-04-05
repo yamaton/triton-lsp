b1f56a47a51f38ac16a6d2f0c2ed63a56c0c6c2c
Yamato Matsuoka <yamato.matsuoka@tritonlab.io>
2022-04-05 15:03:42 -0400

    Use lowercase in coding with sql

--- src/memento.ts
+++ src/memento.ts
@@ -23,14 +23,14 @@ export class Memento {
     } else {
       console.info(`Database file not found;  starting from scratch.`);
       db = Database(path);
-      db.prepare("CREATE TABLE Command (name TEXT, json JSON)").run();
+      db.prepare("create table command (name text, json json)").run();
     }
     return db;
   }
 
 
   public getNames = (): string[] => {
-    return this.db.prepare("SELECT name FROM Command").pluck().all();
+    return this.db.prepare("select name from command").pluck().all();
   }
 
 
@@ -39,7 +39,7 @@ export class Memento {
       return this.cache.get(name);
     }
 
-    const raw = this.db.prepare("SELECT json FROM Command WHERE name=?").pluck().get(name);
+    const raw = this.db.prepare("select json from command where name=?").pluck().get(name);
     const cmd = (!!raw) ? JSON.parse(raw) : undefined;
     if (cmd) {
       this.cache.set(name, cmd);
@@ -49,22 +49,22 @@ export class Memento {
 
 
   public remove = (name: string): void => {
-    this.db.prepare("DELETE FROM Command WHERE name=?").run(name);
+    this.db.prepare("delete from command where name=?").run(name);
     this.cache.delete(name);
     console.info(`Removed ${name} from the database.`)
   }
 
 
   public has = (name: string): boolean => {
-    const ans = this.db.prepare("SELECT name FROM Command WHERE name=?").pluck().get(name);
+    const ans = this.db.prepare("select name from command where name=?").pluck().get(name);
     return (!!ans);
   }
 
 
   public set = (name: string, cmdSpec: Command): void => {
     const stmt = (this.has(name))
-                  ? this.db.prepare("UPDATE Command SET json = @json WHERE name = @name")
-                  : this.db.prepare("INSERT INTO Command VALUES (@name, @json)");
+                  ? this.db.prepare("update command set json = @json where name = @name")
+                  : this.db.prepare("insert into command values (@name, @json)");
     stmt.run({
       name: name,
       json: JSON.stringify(cmdSpec)
@@ -75,7 +75,7 @@ export class Memento {
 
   public loadCommands = (): Map<string, Command> => {
     const d = new Map<string, Command>();
-    const rawdata = this.db.prepare("SELECT json FROM Command").pluck().all();
+    const rawdata = this.db.prepare("select json from command").pluck().all();
     for (const raw of rawdata) {
       const cmd = JSON.parse(raw);
       d.set(cmd.name, cmd);
