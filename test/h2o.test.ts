ddecf66c1fe6291f226f44539576faca863ac717
Yamato Matsuoka <yamaton@gmail.com>
2022-04-08 23:32:52 -0400

    Add h2o test

---
+++ test/h2o.test.ts
@@ -0,0 +1,15 @@
+import chai from "chai";
+import { runH2o } from "../src/h2o";
+
+const assert = chai.assert;
+
+
+describe('runH2o', () => {
+  it("runH2o('h2o')", async () => {
+    const name = 'h2o';
+    const cmd = runH2o(name);
+    const opt = cmd?.options.find(opt => opt.names.some(n => n === '--loadjson'));
+    assert.deepEqual(opt?.description, "Load JSON file in Command schema.");
+  });
+
+});
