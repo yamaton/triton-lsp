27e2fde21c68933d87f91efd045117557f65130d
Yamato Matsuoka <yamato.matsuoka@tritonlab.io>
2022-04-13 14:29:26 -0400

    Update server test

--- test/server.test.ts
+++ test/server.test.ts
@@ -36,38 +36,20 @@ function sendNotification(method: string, params: any) {
 }
 
 
-function initialize(applyEdit: boolean, codeAction?: any, rename?: any): number {
+function initialize(): number {
   return initializeCustomCapabilities({
     textDocument: {
       completion: {
         completionItem: {
-          deprecatedSupport: true,
           documentationFormat: [MarkupKind.Markdown],
-          snippetSupport: true
+          snippetSupport: true,
+          // labelDetailSupport: true,    // [TODO] Enable since 3.17
         }
       },
       hover: {
-        contentFormat: [MarkupKind.PlainText]
+        contentFormat: [MarkupKind.Markdown]
       },
-      codeAction,
-      semanticTokens: {
-        formats: [],
-        requests: {
-          full: {
-            delta: false
-          }
-        },
-        tokenModifiers: [],
-        tokenTypes: []
-      },
-      rename
     },
-    workspace: {
-      applyEdit: applyEdit,
-      workspaceEdit: {
-        documentChanges: true
-      }
-    }
   });
 }
 
@@ -82,258 +64,221 @@ function initializeCustomCapabilities(capabilities: any): number {
 
 
 describe("LSP Tests", () => {
-  it("initialize", function (finished) {
+  it("initialize", function (done) {
     this.timeout(5000);
-    const responseId = initialize(false);
+    const responseId = initialize();
     lspProcess.once('message', function (json: InitializeResult) {
       assert.equal(json.id, responseId);
       let capabilities = json.result.capabilities;
       assert.equal(capabilities.textDocumentSync, TextDocumentSyncKind.Incremental);
-      assert.equal(capabilities.codeActionProvider, false);
-      assert.equal(capabilities.completionProvider.resolveProvider, true);
-      assert.equal(capabilities.executeCommandProvider, undefined);
-      assert.equal(capabilities.foldingRangeProvider, true);
+      assert.equal(capabilities.completionProvider, true);
       assert.equal(capabilities.hoverProvider, true);
-      assert.equal(capabilities.renameProvider, true);
-      assert.equal(capabilities.renameProvider.prepareProvider, undefined);
-      assert.equal(capabilities.semanticTokensProvider.full.delta, false);
-      finished();
+      assert.equal(capabilities.codeActionProvider, undefined);
+      assert.equal(capabilities.foldingRangeProvider, undefined);
+      assert.equal(capabilities.renameProvider, undefined);
+      done();
     });
   });
 
-  it("initialize", function (finished) {
-    this.timeout(5000);
-    const responseId = initialize(false, {}, { prepareSupport: false });
-    lspProcess.once('message', function (json: InitializeResult) {
-      assert.equal(json.id, responseId);
-      let capabilities = json.result.capabilities;
-      assert.equal(capabilities.textDocumentSync, TextDocumentSyncKind.Incremental);
-      assert.equal(capabilities.codeActionProvider, false);
-      assert.equal(capabilities.completionProvider.resolveProvider, true);
-      assert.equal(capabilities.executeCommandProvider, undefined);
-      assert.equal(capabilities.foldingRangeProvider, true);
-      assert.equal(capabilities.hoverProvider, true);
-      assert.equal(capabilities.renameProvider, true);
-      assert.equal(capabilities.renameProvider.prepareProvider, undefined);
-      finished();
-    });
-  });
-
-  it("initialize", function (finished) {
-    this.timeout(5000);
-    const responseId = initialize(false, {}, { prepareSupport: true });
-    lspProcess.once('message', function (json: InitializeResult) {
-      assert.equal(json.id, responseId);
-      let capabilities = json.result.capabilities;
-      assert.equal(capabilities.textDocumentSync, TextDocumentSyncKind.Incremental);
-      assert.equal(capabilities.codeActionProvider, false);
-      assert.equal(capabilities.completionProvider.resolveProvider, true);
-      assert.equal(capabilities.executeCommandProvider, undefined);
-      assert.equal(capabilities.foldingRangeProvider, true);
-      assert.equal(capabilities.hoverProvider, true);
-      assert.equal(capabilities.renameProvider.prepareProvider, true);
-      finished();
-    });
-  });
 
   it("initialized", function () {
     sendNotification("initialized", {});
   });
 
 
-  it("definition", function (finished) {
-    sendNotification("textDocument/didOpen", {
-      textDocument: {
-        languageId: "shellscript",
-        version: 1,
-        uri: "uri://definition.txt",
-        text: "FROM node AS setup"
-      }
-    });
+  // it("definition", function (done) {
+  //   sendNotification("textDocument/didOpen", {
+  //     textDocument: {
+  //       languageId: "shellscript",
+  //       version: 1,
+  //       uri: "uri://definition.txt",
+  //       text: "FROM node AS setup"
+  //     }
+  //   });
 
-    const requestId = sendRequest("textDocument/definition", {
-      textDocument: {
-        uri: "uri://definition.txt",
-      },
-      position: {
-        line: 0,
-        character: 15
-      }
-    });
+  //   const requestId = sendRequest("textDocument/definition", {
+  //     textDocument: {
+  //       uri: "uri://definition.txt",
+  //     },
+  //     position: {
+  //       line: 0,
+  //       character: 15
+  //     }
+  //   });
 
-    const listener = (json) => {
-      if (json.id === requestId) {
-        lspProcess.removeListener("message", listener);
-        assert.strictEqual(json.result.uri, "uri://definition.txt");
-        assert.strictEqual(json.result.range.start.line, 0);
-        assert.strictEqual(json.result.range.start.character, 13);
-        assert.strictEqual(json.result.range.end.line, 0);
-        assert.strictEqual(json.result.range.end.character, 18);
-        finished();
-      }
-    };
-    lspProcess.on("message", listener);
-  });
+  //   const listener = (json) => {
+  //     if (json.id === requestId) {
+  //       lspProcess.removeListener("message", listener);
+  //       assert.strictEqual(json.result.uri, "uri://definition.txt");
+  //       assert.strictEqual(json.result.range.start.line, 0);
+  //       assert.strictEqual(json.result.range.start.character, 13);
+  //       assert.strictEqual(json.result.range.end.line, 0);
+  //       assert.strictEqual(json.result.range.end.character, 18);
+  //       done();
+  //     }
+  //   };
+  //   lspProcess.on("message", listener);
+  // });
 
-  it("formatting", function (finished) {
-    sendNotification("textDocument/didOpen", {
-      textDocument: {
-        languageId: "shellscript",
-        version: 1,
-        uri: "uri://formatting.txt",
-        text: " FROM node AS setup"
-      }
-    });
+  // it("formatting", function (done) {
+  //   sendNotification("textDocument/didOpen", {
+  //     textDocument: {
+  //       languageId: "shellscript",
+  //       version: 1,
+  //       uri: "uri://formatting.txt",
+  //       text: " FROM node AS setup"
+  //     }
+  //   });
 
-    const requestId = sendRequest("textDocument/formatting", {
-      textDocument: {
-        uri: "uri://formatting.txt",
-      },
-      options: {
-        insertSpaces: true,
-        tabSize: 4
-      }
-    });
+  //   const requestId = sendRequest("textDocument/formatting", {
+  //     textDocument: {
+  //       uri: "uri://formatting.txt",
+  //     },
+  //     options: {
+  //       insertSpaces: true,
+  //       tabSize: 4
+  //     }
+  //   });
 
-    const listener = (json) => {
-      if (json.id === requestId) {
-        lspProcess.removeListener("message", listener);
-        assert.ok(json.result instanceof Array);
-        assert.strictEqual(json.result.length, 1);
-        assert.strictEqual(json.result[0].newText, "");
-        assert.strictEqual(json.result[0].range.start.line, 0);
-        assert.strictEqual(json.result[0].range.start.character, 0);
-        assert.strictEqual(json.result[0].range.end.line, 0);
-        assert.strictEqual(json.result[0].range.end.character, 1);
-        finished();
-      }
-    };
-    lspProcess.on("message", listener);
-  });
+  //   const listener = (json) => {
+  //     if (json.id === requestId) {
+  //       lspProcess.removeListener("message", listener);
+  //       assert.ok(json.result instanceof Array);
+  //       assert.strictEqual(json.result.length, 1);
+  //       assert.strictEqual(json.result[0].newText, "");
+  //       assert.strictEqual(json.result[0].range.start.line, 0);
+  //       assert.strictEqual(json.result[0].range.start.character, 0);
+  //       assert.strictEqual(json.result[0].range.end.line, 0);
+  //       assert.strictEqual(json.result[0].range.end.character, 1);
+  //       done();
+  //     }
+  //   };
+  //   lspProcess.on("message", listener);
+  // });
 
-  it("range formatting", function (finished) {
-    sendNotification("textDocument/didOpen", {
-      textDocument: {
-        languageId: "shellscript",
-        version: 1,
-        uri: "uri://range-formatting.txt",
-        text: " FROM node AS setup"
-      }
-    });
+  // it("range formatting", function (done) {
+  //   sendNotification("textDocument/didOpen", {
+  //     textDocument: {
+  //       languageId: "shellscript",
+  //       version: 1,
+  //       uri: "uri://range-formatting.txt",
+  //       text: " FROM node AS setup"
+  //     }
+  //   });
 
-    const requestId = sendRequest("textDocument/rangeFormatting", {
-      textDocument: {
-        uri: "uri://range-formatting.txt",
-      },
-      range: {
-        start: {
-          line: 0,
-          character: 0
-        },
-        end: {
-          line: 0,
-          character: 3
-        }
-      },
-      options: {
-        insertSpaces: true,
-        tabSize: 4
-      }
-    });
+  //   const requestId = sendRequest("textDocument/rangeFormatting", {
+  //     textDocument: {
+  //       uri: "uri://range-formatting.txt",
+  //     },
+  //     range: {
+  //       start: {
+  //         line: 0,
+  //         character: 0
+  //       },
+  //       end: {
+  //         line: 0,
+  //         character: 3
+  //       }
+  //     },
+  //     options: {
+  //       insertSpaces: true,
+  //       tabSize: 4
+  //     }
+  //   });
 
-    const listener = (json) => {
-      if (json.id === requestId) {
-        lspProcess.removeListener("message", listener);
-        assert.ok(json.result instanceof Array);
-        assert.strictEqual(json.result.length, 1);
-        assert.strictEqual(json.result[0].newText, "");
-        assert.strictEqual(json.result[0].range.start.line, 0);
-        assert.strictEqual(json.result[0].range.start.character, 0);
-        assert.strictEqual(json.result[0].range.end.line, 0);
-        assert.strictEqual(json.result[0].range.end.character, 1);
-        finished();
-      }
-    };
-    lspProcess.on("message", listener);
-  });
+  //   const listener = (json) => {
+  //     if (json.id === requestId) {
+  //       lspProcess.removeListener("message", listener);
+  //       assert.ok(json.result instanceof Array);
+  //       assert.strictEqual(json.result.length, 1);
+  //       assert.strictEqual(json.result[0].newText, "");
+  //       assert.strictEqual(json.result[0].range.start.line, 0);
+  //       assert.strictEqual(json.result[0].range.start.character, 0);
+  //       assert.strictEqual(json.result[0].range.end.line, 0);
+  //       assert.strictEqual(json.result[0].range.end.character, 1);
+  //       done();
+  //     }
+  //   };
+  //   lspProcess.on("message", listener);
+  // });
 
-  it("on type formatting", (finished) => {
-    sendNotification("textDocument/didOpen", {
-      textDocument: {
-        languageId: "shellscript",
-        version: 1,
-        uri: "uri://on-type-formatting.txt",
-        text: "FROM node AS setup\nRUN echo \necho"
-      }
-    });
+  // it("on type formatting", (done) => {
+  //   sendNotification("textDocument/didOpen", {
+  //     textDocument: {
+  //       languageId: "shellscript",
+  //       version: 1,
+  //       uri: "uri://on-type-formatting.txt",
+  //       text: "FROM node AS setup\nRUN echo \necho"
+  //     }
+  //   });
 
-    const requestId = sendRequest("textDocument/onTypeFormatting", {
-      textDocument: {
-        uri: "uri://on-type-formatting.txt",
-      },
-      position: {
-        line: 1,
-        character: 9
-      },
-      ch: '\\',
-      options: {
-        insertSpaces: true,
-        tabSize: 4
-      }
-    });
+  //   const requestId = sendRequest("textDocument/onTypeFormatting", {
+  //     textDocument: {
+  //       uri: "uri://on-type-formatting.txt",
+  //     },
+  //     position: {
+  //       line: 1,
+  //       character: 9
+  //     },
+  //     ch: '\\',
+  //     options: {
+  //       insertSpaces: true,
+  //       tabSize: 4
+  //     }
+  //   });
 
-    const listener = (json) => {
-      if (json.id === requestId) {
-        lspProcess.removeListener("message", listener);
-        assert.ok(json.result instanceof Array);
-        assert.strictEqual(json.result.length, 1);
-        assert.strictEqual(json.result[0].newText, "    ");
-        assert.strictEqual(json.result[0].range.start.line, 2);
-        assert.strictEqual(json.result[0].range.start.character, 0);
-        assert.strictEqual(json.result[0].range.end.line, 2);
-        assert.strictEqual(json.result[0].range.end.character, 0);
-        finished();
-      }
-    };
-    lspProcess.on("message", listener);
-  });
+  //   const listener = (json) => {
+  //     if (json.id === requestId) {
+  //       lspProcess.removeListener("message", listener);
+  //       assert.ok(json.result instanceof Array);
+  //       assert.strictEqual(json.result.length, 1);
+  //       assert.strictEqual(json.result[0].newText, "    ");
+  //       assert.strictEqual(json.result[0].range.start.line, 2);
+  //       assert.strictEqual(json.result[0].range.start.character, 0);
+  //       assert.strictEqual(json.result[0].range.end.line, 2);
+  //       assert.strictEqual(json.result[0].range.end.character, 0);
+  //       done();
+  //     }
+  //   };
+  //   lspProcess.on("message", listener);
+  // });
 
-  it("rename", (finished) => {
-    sendNotification("textDocument/didOpen", {
-      textDocument: {
-        languageId: "shellscript",
-        version: 1,
-        uri: "uri://rename.txt",
-        text: "FROM node AS setup"
-      }
-    });
+  // it("rename", (done) => {
+  //   sendNotification("textDocument/didOpen", {
+  //     textDocument: {
+  //       languageId: "shellscript",
+  //       version: 1,
+  //       uri: "uri://rename.txt",
+  //       text: "FROM node AS setup"
+  //     }
+  //   });
 
-    const requestId = sendRequest("textDocument/rename", {
-      textDocument: {
-        uri: "uri://rename.txt",
-      },
-      position: {
-        line: 0,
-        character: 15
-      },
-      newName: "build"
-    });
+  //   const requestId = sendRequest("textDocument/rename", {
+  //     textDocument: {
+  //       uri: "uri://rename.txt",
+  //     },
+  //     position: {
+  //       line: 0,
+  //       character: 15
+  //     },
+  //     newName: "build"
+  //   });
 
-    const listener = (json) => {
-      if (json.id === requestId) {
-        lspProcess.removeListener("message", listener);
-        const changes = json.result.changes["uri://rename.txt"];
-        assert.ok(changes instanceof Array);
-        assert.strictEqual(changes.length, 1);
-        assert.strictEqual(changes[0].newText, "build");
-        assert.strictEqual(changes[0].range.start.line, 0);
-        assert.strictEqual(changes[0].range.start.character, 13);
-        assert.strictEqual(changes[0].range.end.line, 0);
-        assert.strictEqual(changes[0].range.end.character, 18);
-        finished();
-      }
-    };
-    lspProcess.on("message", listener);
-  });
+  //   const listener = (json) => {
+  //     if (json.id === requestId) {
+  //       lspProcess.removeListener("message", listener);
+  //       const changes = json.result.changes["uri://rename.txt"];
+  //       assert.ok(changes instanceof Array);
+  //       assert.strictEqual(changes.length, 1);
+  //       assert.strictEqual(changes[0].newText, "build");
+  //       assert.strictEqual(changes[0].range.start.line, 0);
+  //       assert.strictEqual(changes[0].range.start.character, 13);
+  //       assert.strictEqual(changes[0].range.end.line, 0);
+  //       assert.strictEqual(changes[0].range.end.character, 18);
+  //       done();
+  //     }
+  //   };
+  //   lspProcess.on("message", listener);
+  // });
 
 });
